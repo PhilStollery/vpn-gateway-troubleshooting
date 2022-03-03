c3a48ed7dbd65c39bf50a2472dea32401d3b43da
Phil <phils@contentmaster.com>
2022-03-03 16:07:21 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 resource=`az group list --query '[0].name' --output tsv`
 
+echo "Creating VM Scale Set in $resource..."
 az vmss create --resource-group $resource --name myScaleSet --image UbuntuLTS --upgrade-policy-mode automatic --admin-username azureuser --generate-ssh-keys
+echo "Setting up webservers..."
 az vmss extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --resource-group $resource --vmss-name myScaleSet --settings '{"fileUris":["https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate_nginx.sh"],"commandToExecute":"./automate_nginx.sh"}'
-az network lb rule create --resource-group $resource --name myLoadBalancerRuleWeb --lb-name myScaleSetLB --backend-pool-name myScaleSetLBBEPool --backend-port 80 --frontend-ip-name loadBalancerFrontEnd --frontend-port 80 --protocol tcp
+echo "Allowing port 80 to servers..."
+az network lb rule create --resource-group $resource --name myLoadBalancerRuleWeb --lb-name myScaleSetLB --backend-pool-name myScaleSetLBBEPool --frontend-port 80 --backend-port 443 --frontend-ip-name loadBalancerFrontEnd  --protocol tcp
